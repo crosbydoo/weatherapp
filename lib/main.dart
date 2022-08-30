db6941e78aa392eacefa433c406bdb756d3b16c9
rahmiwelsifa <rahmiwelsifaaprila@gmail.com>
2022-08-30 11:40:23 +0700

    commmit terbaru

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,25 @@
-// ignore_for_file: prefer_typing_uninitialized_variables, duplicate_ignore
+// ignore_for_file: prefer_typing_uninitialized_variables, duplicate_ignore, import_of_legacy_library_into_null_safe
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
-void main() => runApp(
-  const MaterialApp(
-    title: "Weather App",
-    home: Home(),
-  )
-);
+void main() => runApp(const MaterialApp(
+      title: "Weather App",
+      home: Home(),
+    ));
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState (){
+  State<StatefulWidget> createState() {
     return _HomeState();
   }
 }
 
 class _HomeState extends State<Home> {
-
   // ignore: prefer_typing_uninitialized_variables
   var temp;
   // ignore: prefer_typing_uninitialized_variables
@@ -39,9 +35,9 @@ class _HomeState extends State<Home> {
   var sunrise;
   var sunset;
 
-
-  Future getWeather () async{
-    http.Response response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=-7.89145945&lon=110.33614768316843&units=metric&appid=e42b9722d62b644a3b13549d36292666'));
+  Future getWeather() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=-7.89145945&lon=110.33614768316843&units=metric&appid=e42b9722d62b644a3b13549d36292666'));
     var results = jsonDecode(response.body);
     setState(() {
       temp = results['main']['temp'];
@@ -56,13 +52,13 @@ class _HomeState extends State<Home> {
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     getWeather();
   }
 
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
@@ -71,42 +67,38 @@ class _HomeState extends State<Home> {
             width: MediaQuery.of(context).size.width,
             color: Colors.blue,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 10.0),
-                  child: Text(
-                    // ignore: unnecessary_null_comparison
-                    "Currently" + city != null ? city.toString() : "Loading",
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 14.8,
-                      fontWeight: FontWeight.w600
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Text(
+                      // ignore: unnecessary_null_comparison
+                      "Currently" + city != null ? city.toString() : "Loading",
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 14.8,
+                          fontWeight: FontWeight.w600),
                     ),
                   ),
-                ),
-                Text(
-                  temp != null ? temp.toString() + "\u0000" : "Loading",
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w600
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    currently != null ? currently.toString() : "Loading",
+                  Text(
+                    temp != null ? temp.toString() + "\u0000" : "Loading",
                     style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w600
-                    ),
+                        color: Colors.white,
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w600),
                   ),
-                )
-              ]
-            ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      currently != null ? currently.toString() : "Loading",
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  )
+                ]),
           ),
           Expanded(
             child: Padding(
@@ -117,25 +109,32 @@ class _HomeState extends State<Home> {
                     // ignore: deprecated_member_use
                     leading: const FaIcon(FontAwesomeIcons.thermometerHalf),
                     title: const Text('Temperature'),
-                    trailing: Text(temp != null ? temp.toString() + "\u00B0C" : "Loading"),
+                    trailing: Text(
+                        temp != null ? temp.toString() + "\u00B0C" : "Loading"),
                   ),
                   ListTile(
                     // ignore: deprecated_member_use
                     leading: const FaIcon(FontAwesomeIcons.cloud),
                     title: const Text('Weather'),
-                    trailing: Text(description != null ? description.toString() : "Loading"),
+                    trailing: Text(description != null
+                        ? description.toString()
+                        : "Loading"),
                   ),
                   ListTile(
                     // ignore: deprecated_member_use
                     leading: const FaIcon(FontAwesomeIcons.sun),
                     title: const Text('Humidity'),
-                    trailing: Text(humidity != null ? humidity.toString() + "%" : "Loading"),
+                    trailing: Text(humidity != null
+                        ? humidity.toString() + "%"
+                        : "Loading"),
                   ),
                   ListTile(
                     // ignore: deprecated_member_use
                     leading: const FaIcon(FontAwesomeIcons.wind),
                     title: const Text('Wind Speed'),
-                    trailing: Text(windspeed != null ? windspeed.toString() + " m/s": "Loading"),
+                    trailing: Text(windspeed != null
+                        ? windspeed.toString() + " m/s"
+                        : "Loading"),
                   ),
                 ],
               ),
